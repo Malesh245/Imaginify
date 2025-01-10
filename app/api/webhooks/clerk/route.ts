485abcdd5c34b67867ab0aa1980dccb397b56e2d
Malesh Kumar <maleshkumar12345@gmail.com>
2025-01-10 14:34:02 +0530

    update

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -176,7 +176,7 @@ export async function POST(req: Request) {
     const { email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      clerkId: id,
+      clerkId: id as string,
       email: email_addresses[0].email_address,
       username: username || "",
       firstName: first_name,
